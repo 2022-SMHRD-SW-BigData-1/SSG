72e0938551070f8bfc7f7af94ad80d6928a88c73
jay <daonj0715@gmail.com>
2023-02-10 14:08:58 +0900

    a

---
+++ src/pages/Graf.js
@@ -0,0 +1,28 @@
+// import { useCallback } from "react";
+// import { useNavigate } from "react-router-dom";
+import "./Graf.css";
+import Barchart from "../components/Barch";
+import Barchart1 from "../components/Barch1";
+import Navs from "../components/Navs";
+
+const Graf = () => {
+  return (
+    <div className="graf">
+      <Navs />
+      <div className="total-revenue-wrapper">
+        <div className="total-revenue">
+          <div className="allgraf">
+            <Barchart />
+            {/* <h1 className="text1">사고 발생율</h1> */}
+          </div>
+          <div className="graf2">
+            <Barchart1 />
+            {/* <h1 className="text2">차량 통행량</h1> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Graf;
